227362edc504f60642d620b42360bcec26bcd1d9
ibrkhalil <vampirekid017@gmail.com>
2024-06-22 16:20:57 +0300

    cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,7 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
+  const p = screen.getByText(/and save to reload/)
+  expect(p.innerHTML).toContain('save to reload')
   expect(linkElement).toBeInTheDocument();
 });
